308d1f4b18d243760c22e14a0db0dc04f3cbc551
Kumar <2494584@cognizant.com>
2026-05-22 14:24:53 +0530

    feat: complete landing page redesign with features, how-it-works, reviews, CTA, conditional nav

--- HealthcareManagement-Frontend/src/app/components/welcomepage/welcomepage.component.ts
+++ HealthcareManagement-Frontend/src/app/components/welcomepage/welcomepage.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-welcomepage',
@@ -8,15 +9,29 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class WelcomepageComponent implements OnInit {
 
-  constructor(private activatedRoute: ActivatedRoute,private _router : Router) { }
+  isLoggedIn = false;
+  userRole = '';
+  userName = '';
 
-  ngOnInit(): void 
-  {
-    
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private _router: Router,
+    private _loginService: LoginService
+  ) { }
+
+  ngOnInit(): void {
+    this.isLoggedIn = this._loginService.isLoggedIn();
+    this.userRole   = this._loginService.userType();
+    this.userName   = localStorage.getItem('name') || '';
   }
 
-  navigate()
-  {
+  goToDashboard() {
+    if (this.userRole === 'admin')       this._router.navigate(['/admindashboard']);
+    else if (this.userRole === 'doctor') this._router.navigate(['/doctordashboard']);
+    else                                  this._router.navigate(['/userdashboard']);
+  }
+
+  navigate() {
     this._router.navigate(['/login']);
   }
 }
